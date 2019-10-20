70b231dda35b488e2ba0989f53fdd1fa9c528a86
Anwesh43 <anweshthecool0@gmail.com>
2019-10-20 13:27:40 +0530

    adding fullscreen support

--- app/src/main/java/com/anwesh/uiprojects/linkedjumpballwithlineview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedjumpballwithlineview/MainActivity.kt
@@ -2,6 +2,7 @@ package com.anwesh.uiprojects.linkedjumpballwithlineview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
 import com.anwesh.uiprojects.jumpballlineview.JumpBallLineView
 
 class MainActivity : AppCompatActivity() {
@@ -9,5 +10,11 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         JumpBallLineView.create(this)
+        fullScreen()
     }
+}
+
+fun MainActivity.fullScreen() {
+    supportActionBar?.hide()
+    window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
 }
