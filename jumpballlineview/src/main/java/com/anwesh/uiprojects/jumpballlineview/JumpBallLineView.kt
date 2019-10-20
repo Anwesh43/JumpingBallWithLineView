51159e26b459770381983a08b18a849eb6ea1bc3
Anwesh43 <anweshthecool0@gmail.com>
2019-10-20 12:56:23 +0530

    adding Int and Float extension method

--- jumpballlineview/src/main/java/com/anwesh/uiprojects/jumpballlineview/JumpBallLineView.kt
+++ jumpballlineview/src/main/java/com/anwesh/uiprojects/jumpballlineview/JumpBallLineView.kt
@@ -19,3 +19,7 @@ val delay : Long = 20
 val foreColor : Int = Color.parseColor("#9FA8DA")
 val backColor : Int = Color.parseColor("#BDBDBD")
 
+fun Int.inverse() : Float = 1f / this
+fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
+fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+fun Float.toDest(source : Float, dest : Float) : Float = source + (dest - source) * this
