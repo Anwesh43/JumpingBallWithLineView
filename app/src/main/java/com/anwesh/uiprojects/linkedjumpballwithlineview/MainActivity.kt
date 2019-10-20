259cc663e318e7435dc8a35fa61405cbf0b44f3f
Anwesh43 <anweshthecool0@gmail.com>
2019-10-20 13:27:02 +0530

    adding view in activity

--- app/src/main/java/com/anwesh/uiprojects/linkedjumpballwithlineview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedjumpballwithlineview/MainActivity.kt
@@ -2,11 +2,12 @@ package com.anwesh.uiprojects.linkedjumpballwithlineview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.anwesh.uiprojects.jumpballlineview.JumpBallLineView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        JumpBallLineView.create(this)
     }
 }
